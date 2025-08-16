27ba4682542b9624d86446ecd213df9bc8c43405
Jakub Radzik <jradzik4@gmail.com>
2025-08-16 11:44:38 +0200

    fix

--- api/daily-post.ts
+++ api/daily-post.ts
@@ -1,7 +1,16 @@
 import type { VercelRequest, VercelResponse } from "@vercel/node";
 import { main } from "../src/main";
+import config from "../src/config";
+
+const { CRON_SECRET } = config;
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
+  const authHeader = req.headers["authorization"];
+
+  if (!CRON_SECRET || authHeader !== `Bearer ${process.env.CRON_SECRET}`) {
+    return res.status(401).json({ success: false });
+  }
+
   try {
     await main();
     res.status(200).send("Image sent!");
--- src/config/index.ts
+++ src/config/index.ts
@@ -5,4 +5,5 @@ dotenv.config();
 export default {
   BOT_TOKEN: process.env.BOT_TOKEN,
   CHANNEL_ID: process.env.CHANNEL_ID,
+  CRON_SECRET: process.env.CRON_SECRET,
 };
